60b6c23789962cad373f27a377a31242e8967c0d
Asriann15 <asriann9@gmail.com>
2022-04-21 11:44:13 +0800

    commit

---
+++ src/main/Quis1/Muhammad Asriannnoor.kt
@@ -0,0 +1,50 @@
+/*
+1. Menurut Website yang saya baca unit testing perlu dilakukan  dalam pengembangan dari suatu software.
+agar seorang software engineer dapat menemukan error dan adanya bug dalam software, dan juga apakah software dapat berjalan
+seperti apa yang diharapkan. https://socs.binus.ac.id/2020/06/30/software-testing/
+dan tambahan dari saya: menurut saya pengimplementasian software testing ini opsional untuk dilakukan karena yang pertama
+adalah dengan melakukan perkiraan dengan melihat skala projek atau unit yang dibuat dulu apakah perlu dilakukan
+unit testing dan jika itu projek skala besar maka menurut saya perlu dilakukan software testing dan sebaliknya jika
+hanya projek yang kecil dan menengah boleh-boleh saja untuk tidak melakukan unit testing
+
+2. https://www.labana.id/view/memahai-software-testing-unit-test-integration-test-apa-perlu-dan-dampaknya/2016/06/27/?fullview
+yang akan terjadi jika tidak dilakukan unit testing adalah adanya bug yang akan menyebabkan software tidak berjalan. sedangkan
+jika tidak dilakukan integration testing maka ada kemungkinan software tidak berjalan karena adanya error pada luar software
+yang dimana ini berhubungan dengan REST API atau mungkin berhubungan dengan service Thrift.
+
+3. https://medium.com/@makersinstitute/mengenal-lebih-jauh-tentang-software-quality-assurance-844361cd50db
+melihat dari situs ini saya beranggapan jika QA Engineer memiliki tugas yang berbeda dari software engineer
+yang dimana software engineer hannya melakukan unit test pada software dan QA Engineer bertugas untuk melakukan
+pengujian software dengan, membuat program pengujian otomatis, dan membuat laporan terkait hasil pengujian, serta memberi
+masukan pada software yang diuji, dan QAA Engineer juga berkomunukasi dengan pihak penting seperti pengembang UI/UX,
+pengembang back end atau product manager. jadi menurut saya QA Engineer memiliki tugas yang berbeda dari Software Engineer
+dan diperlukan pada masa pembuatan software.
+
+4. Dari web yang saya temukan seperti
+https://kennel209.gitbooks.io/owasp-testing-guide-v4/content/en/web_application_security_testing/test_business_logic_data_validation_otg-buslogic-001.html
+https://glints.com/id/lowongan/usability-testing-adalah/#.YmC7TNpBzIU
+https://developer.android.com/training/testing/ui-testing?hl=id
+
+test business logic adalah test yang dimana software harus dipastikan bahwa data valid secara logis dan menjadi sistem yang mampu memandu
+hubungan semua objek yang bekerja dalam sistem.
+
+Dan Test UI/UX adalah test yang dimana software harus dipastikan bahwa data valid secara visual
+berperilaku seperti yang diharapkan saat pengguna melakukan tindakan tertentu atau memasukkan input tertentu dalam aktivitasnya.
+agar mudah bagi user yang akan menggunakan software tersebut.
+
+Keamanan adalah hal yang harus dilakukan oleh developer untuk menjaga keamanan dari software yang dibuat.
+
+lalu pendapat saya tentang soal ini adalah: jika test tersebut tidak diimplementasikan maka dapat ditebak aplikasi akan mengalami banyak kesalahan
+seperti jika tidak melakukan busines logic maka sistem akan berjalan dengan tidak terpadu. dan jika tidak melakukan UI/UX test maka yang akan terjadi
+contohnya seperti dimana interface akan menampilkan kesalahan seperti perilaku yang tidak semestinya berjalan tidak sesuai keinginan user dan
+mengganggu pengguna. dan juga apabila tidak melakukan test keamanan maka software akan mengalami kecurangan dan berkurangnya kepercayaan user terhadap
+Software.
+
+5.https://socs.binus.ac.id/2020/06/30/software-testing/
+Software testing merupakan salah satu bagian penting dan sering digunakan dalam pengembangan dari suatu software.
+dan Software dapat dilakukan secara manual atau otomatis. kebanyakan tester menggunakan Test secara otomatis, dan tidak banyak juga tester melakukan
+secara manual dan pertanyaannya apa perlu kita melakukan manual testing pada softaware yang sudah dilakukan secara otomatis test.
+
+jawaban saya perlu dilakukan tes secara manual karena manual tes dilakukan tanpa tools, perlu banyak waktu dan langkah akan tetapi
+cara ini efektif karena karena mampu mendeteksi bug yang tidak dilihat oleh tools auto test.
+ */
