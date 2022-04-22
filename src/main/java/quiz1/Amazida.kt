15ca54408f3fa1c8557bd070b82ca15c7c8fc377
zidaaja <2010817120002>
2022-04-22 13:17:43 +0700

    no 3 belum

--- src/main/java/quiz1/Amazida.kt
+++ src/main/java/quiz1/Amazida.kt
@@ -5,11 +5,17 @@ class Amazida {
 
 1. Dewasa ini, pembuatan testing pada perangkat lunak umum dilakukan pada saat tahap implementasi yang dilakukan oleh software
 engineer. Berikan opini kalian apakah software engineer pada tahap implementasi wajib melakukan test atau tidak?
+jawaban : Menurut saya melakukan test wajib dilakukan karena untuk mengetahui apakah software tersebut sudah sesuai dengan yang diharapkan atau tidak.
+Jika tidak melalukan testing, kesalahan apapun dapat terjadi pada software tersebut.
 
 2. Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi. berikan pendapat kalian jika dua test
 ini tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
+jawaban : unit test dan integration test sebenarnya sangat berpengaruh pada kualitas software yang dibuat.
+jika software testing ini tidak dilakukan, maka akan berdampak kualitas yang buruk pada software, mungkin akan terdapat bug
+dan lain sebagainya jika telah sampai ditangan end-users.
 
 3. Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer telah melakukan testing
+jawaban :
 
 4. Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan. Namun tidak
 semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan. Berikan pendapat kalian dampak
