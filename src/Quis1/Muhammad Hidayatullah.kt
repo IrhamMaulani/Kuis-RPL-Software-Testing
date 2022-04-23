9e6b89336c45a20dddff7bd14afc936be7733392
muhammadhidayatullah17 <2010817110012@mhs.ulm.ac.id>
2022-04-24 05:20:56 +0800

    Commit dayat

--- src/Quis1/Muhammad Hidayatullah.kt
+++ src/Quis1/Muhammad Hidayatullah.kt
@@ -2,7 +2,7 @@
 1. Dewasa ini, pembuatan testing pada perangkat lunak umum dilakukan pada saat tahap implementasi yang dilakukan oleh software engineer. Berikan opini kalian apakah software engineer pada tahap implementasi wajib melakukan test atau tidak?
 Jawab : Test
 2. Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi. berikan pendapat kalian jika dua test ini tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
-
+Jawab : Test Ini Fork
 3. Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer telah melakukan testing
 
 4. Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan. Namun tidak semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan. Berikan pendapat kalian dampak apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
