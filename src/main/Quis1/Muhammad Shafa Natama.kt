a3b9e250c05a08f599ee03474f23b18a24200718
MSNatama <101536511+MSNatama@users.noreply.github.com>
2022-04-25 00:25:56 +0800

    Quiz 1 Shafa

---
+++ src/main/Quis1/Muhammad Shafa Natama.kt
@@ -0,0 +1,41 @@
+
+
+# Kuis 1 (90)
+1. Dewasa ini, pembuatan testing pada perangkat lunak umum dilakukan pada saat tahap implementasi yang dilakukan oleh software
+engineer. Berikan opini kalian apakah software engineer pada tahap implementasi wajib melakukan test atau tidak?
+
+2. Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi. berikan pendapat kalian jika dua test
+ini tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
+
+3. Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer telah melakukan testing
+
+4. Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan. Namun tidak
+semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan. Berikan pendapat kalian dampak
+apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
+
+5. Berikan pendapat kalian tentang testing perangkat lunak secara manual? Apakah di saat banyaknya testing secara otomatisasi dilakukan testing secara
+manual perlu dilakukan?
+
+#Jawaban Kuis 1
+1. Berdasarkan sumber dari https://archerpoint.com/importance-software-testing-during-implementation/ menurut saya testing pada tahap Implementasi
+wajib dilakukan karena kita dapat mudah mengetahui dimana salah pada sebuah software daripada melakukan
+testing di tahap selanjutnya yang akan menyulitkan kita dalam mengidentifikasi kesalahan tersebut.
+
+2. berdasarkan
+
+3. Bedasarkan sumber dari https://techbeacon.com/app-dev-testing/qa-necessary-or-should-developers-do-their-own-testing
+Menurut saya, QA Engineer dapat meningkatkan hasil software karena test yang hanya menggunakan kode kurang human factornya.
+Manusia biasanya sering mencoba-coba hal unik dengan aplikasi yang terkadang mengejutkan. Dalam hal tersebut QA Engineer
+meningkatkan kesuksesan tes kode dengan menyediakan elemen manusia untuk menghindari kejadian tersebut.
+
+4. Berdasarkan sumber dari https://www.indiumsoftware.com/blog/why-software-testing/
+Pendapat saya, dengan tidak melakukan testing yang tertera diatas perangkat lunak akan memungkinkan membuang lebih banyak
+uang dan waktu dikarenakan apabila ditemukan bug pada saat perangkat lunak di maka diperlukan untuk memperbaikinya belum
+lagi kita mengikutkan reputasi yang bisa membuat menurunnya jumlah pengguna. Terutama tidak melakukan Security testing
+yang merupakan salah satu poin penting dalam software testing, apabila data pengguna dicuri akan membahayakan pengguna dan
+mengakibatkan kehilangannya kepercayaan pengguna lainnya.
+
+5. Menurut website https://smartbear.com/blog/why-manual-testing-is-still-important/#:~:text=Many%20teams%20are%20exploring%20how,The%20resounding%20answer%20is%20yes.
+Berdasarkan website tersebut dapat dikatakan bahwa manual testing masih relevan dan diperlukan
+
+*/
