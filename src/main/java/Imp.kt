b946d1cfe8a6b638f6c99843a8e3591cfd72813d
Al Chatra Syarifudin <101439279+Chatra11@users.noreply.github.com>
2022-04-24 20:55:14 +0800

    fungsi main array list

--- src/main/java/Imp.kt
+++ src/main/java/Imp.kt
@@ -1,4 +1,14 @@
 import java.util.*
 
 fun main(args: Array<String>) {
+    var student:Student =Student()
+    println("Nama: ${student.nama}")
+    println("ID: ${student.id}")
+    println("Tanggal lahir : ${student.tanggal}")
+    println("Nomor HP : ${student.nomor}")
+    println("Tanggal Lahir setelah format: ${student.dateFormat()}")
+    println("NIM : ${student.getNim()}")
+    println("Hobi : ${student.tampilhobi()}")
+    println("Ayah : ${student.Orangtua.ayah}")
+    println("Ibu : ${student.Orangtua.ibu}")
 }
