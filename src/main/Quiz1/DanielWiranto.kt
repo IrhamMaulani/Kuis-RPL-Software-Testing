b71bbbcafa73a812228f80f3f437a0fdcd2690bc
Daniel.W <101545435+DanielWiranto@users.noreply.github.com>
2022-04-24 22:52:37 +0800

    Add Quiz1

---
+++ src/main/Quiz1/DanielWiranto.kt
@@ -0,0 +1,16 @@
+3.QA engineer dan software engineer dalam melakukan testing sama-sama memastikan perangkat berjalan dengan baik.
+
+Software Engineer mengeksplorasi cara untuk memeriksa cara kerja aplikasi dan menemukan kemungkinan cacat.
+Berbagai metode dan metodologi pengujian digunakan oleh penguji untuk menguji produk, menemukan bug, dan
+memeriksa apakah telah diperbaiki. Pengujian memungkinkan pelanggan dengan kemungkinan untuk melihat apakah
+produk yang dikembangkan memenuhi harapan mereka pada desain, kompatibilitas, fungsi, dll. Fokus utama
+Penguji adalah menguji aplikasi dan menemukan semua kemungkinan bug yang dapat merusak aplikasi.
+
+Sementara di sisi lain, QA Engineer mengikuti serangkaian metode dan aktivitas yang dirancang untuk
+memastikan bahwa aplikasi perangkat lunak sesuai standar atau SRS (Spesifikasi Kebutuhan Perangkat Lunak)
+dan sesuai dengan semua spesifikasi. Ini adalah strategi yang direncanakan dari evaluasi proses pengujian
+yang ditujukan untuk hasil produk yang berkualitas. Insinyur QA mencari cara untuk mencegah kemungkinan bug
+dalam proses pengembangan perangkat lunak. Mereka juga menangani hal-hal manajemen seperti metode dan teknik
+pengembangan, analisis proyek, daftar periksa, dll. QA melewati seluruh siklus hidup produk (SDLC) dan memimpin
+proses pemeliharaan perangkat lunak. Fokus utama mereka adalah Kontrol Kualitas dan untuk memastikan
+persyaratan terpenuhi dengan benar.
