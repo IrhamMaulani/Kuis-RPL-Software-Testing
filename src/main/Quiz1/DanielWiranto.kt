fd547279550e1267bc75003ab52f27186bda5621
Daniel.W <101545435+DanielWiranto@users.noreply.github.com>
2022-04-24 23:42:35 +0800

    Add Jawab Quiz1

--- src/main/Quiz1/DanielWiranto.kt
+++ src/main/Quiz1/DanielWiranto.kt
@@ -1,16 +1,51 @@
-3.QA engineer dan software engineer dalam melakukan testing sama-sama memastikan perangkat berjalan dengan baik.
+/*
 
-Software Engineer mengeksplorasi cara untuk memeriksa cara kerja aplikasi dan menemukan kemungkinan cacat.
-Berbagai metode dan metodologi pengujian digunakan oleh penguji untuk menguji produk, menemukan bug, dan
-memeriksa apakah telah diperbaiki. Pengujian memungkinkan pelanggan dengan kemungkinan untuk melihat apakah
-produk yang dikembangkan memenuhi harapan mereka pada desain, kompatibilitas, fungsi, dll. Fokus utama
-Penguji adalah menguji aplikasi dan menemukan semua kemungkinan bug yang dapat merusak aplikasi.
+1. Dewasa ini, pembuatan testing pada perangkat lunak umum dilakukan pada saat tahap implementasi yang dilakukan
+oleh software engineer. Berikan opini kalian apakah software engineer pada tahap implementasi wajib melakukan
+test atau tidak?
 
-Sementara di sisi lain, QA Engineer mengikuti serangkaian metode dan aktivitas yang dirancang untuk
-memastikan bahwa aplikasi perangkat lunak sesuai standar atau SRS (Spesifikasi Kebutuhan Perangkat Lunak)
-dan sesuai dengan semua spesifikasi. Ini adalah strategi yang direncanakan dari evaluasi proses pengujian
-yang ditujukan untuk hasil produk yang berkualitas. Insinyur QA mencari cara untuk mencegah kemungkinan bug
-dalam proses pengembangan perangkat lunak. Mereka juga menangani hal-hal manajemen seperti metode dan teknik
-pengembangan, analisis proyek, daftar periksa, dll. QA melewati seluruh siklus hidup produk (SDLC) dan memimpin
-proses pemeliharaan perangkat lunak. Fokus utama mereka adalah Kontrol Kualitas dan untuk memastikan
-persyaratan terpenuhi dengan benar.
+2. Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi. berikan pendapat kalian
+jika dua test ini tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
+
+3. Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer telah melakukan testing
+
+4. Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan. Namun
+tidak semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan. Berikan pendapat
+kalian dampak apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
+
+5. Berikan pendapat kalian tentang testing perangkat lunak secara manual? Apakah di saat banyaknya testing secara
+otomatisasi dilakukan testing secara manual perlu dilakukan?
+
+JAWABAN
+
+1. Testing pada perangkat lunak wajib dilakukan karena Tujuan utama dari pengujian perangkat lunak sebenarnya ,
+yaitu untuk memastikan bahwa software yang dihasilkan sesuai dengan kebutuhan (requirement) yang sebelumnya ditentukan.
+Dan manfaatnya adalah Menemukan kesalahan (fault) sebanyak mungkin dari perangkat lunak yang diuji. Jika testing tidak
+dilakukan maka kemungkinan akan terjadi maka error atau bug saat perangkat lunak dijalankan sehingga menyebabkan kualitas
+perangkat lunak menjadi buruk
+
+2. Jika 2 test tersebut tidak dilakukan maka banyak cacat yang akan ditemukan dalam perangkat lunak dan performa
+perangkat lunak akan menurun drastis
+
+3. QA engineer dan software engineer dalam melakukan testing sama-sama memastikan perangkat berjalan dengan baik.
+Menurut saya perbedaannya yaitu Software Engineer lebih berfokus pada hasil akhir perangkat lunak yang dibuat.
+Sedangkan QA Engineer memperhatikan setiap awal, proses dan akhir dari perangkat lunak secara mendetail.
+
+4. Jika testing tidak dilakukan maka akan terjadi kegagalan pada perangkat lunak
+
+5. Menurut saya testing perangkat lunak secara manual sangatlah penting, karena dari pengujian secara manual
+tester bisa menguji semua fitur aplikasi apakah bekerja dengan benar.
+
+Sumber
+1. https://biztechacademy.id/manfaat-pengujian-perangkat-lunak/
+
+2. https://www.softwaretestinghelp.com/unit-testing/#Importance_of_Writing_Unit_Tests
+
+3. https://www.quora.com/What-is-the-difference-between-a-QA-engineer-and-Software-Test-engineer
+
+4. https://www.google.com/search?rlz=1C1AVFC_enID885ID885&sxsrf=APq-WBv33QT9T1rRj5MUlae1QKoFDqTroA:
+1650814173982&q=Contoh+kegagalan+perangkat+lunak+karena+kurangnya+perhatian+pada+proses+pengujian&sa=
+X&ved=2ahUKEwiLsLiDgq33AhXnIbcAHVWsAQoQ1QJ6BAhHEAE&biw=1536&bih=792&dpr=1.25
+
+5. https://socs.binus.ac.id/2020/06/30/software-testing/
+ */
