e9839a60996cf79d0320c46ecc91bc4541fd7a24
Crunchii <2010817210011@mhs.ulm.ac.id>
2022-04-24 16:10:25 +0800

    Pengisian Student Class dan Pengimplementasiannya

--- src/main/java/Imp.kt
+++ src/main/java/Imp.kt
@@ -1,4 +1,20 @@
 import java.util.*
 
 fun main(args: Array<String>) {
+    var student:Student = Student()
+    println("ID: ${student.studentid}")
+    println("Nama: ${student.nama}")
+    println("Tanggal Lahir: ${student.tlahir}")
+    println("Nomor HP: ${student.nohp}")
+    println()
+    println("Tanggal Lahir (reformat) : ${student.tanggal()}")
+    println("Conditional Nomor HP : ${student.Phonenum()}")
+
+    val ortu = Student.Orangtua() // create l1 object of Lamp class
+    ortu.displayortu()
+    print("Hobi : ")
+    println("${student.arrayhobi()}")
+
+    println("NIM : ${student.genNIM()}")
+
 }
