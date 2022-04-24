a1d33214268f825a8284a530c8a154e62c6de1be
Salsabila Syifa <2010817320004@mhs.ulm.ac.id>
2022-04-24 19:26:37 +0800

    update kuis2 dan mengunggah jawaban kuis1

--- src/main/java/Student.kt
+++ src/main/java/Student.kt
@@ -1,28 +1,47 @@
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
 class Student {
     // Impelementasikan properties dan fungsi sesuai spesifikasi dibawah
     // Dan buatkan test pada setiap fungsi yang memastikan bahwa fungsi berjalan baik.
 
+    var id = Locale("in", "ID")
+
     // properties student berisikan :
 
     // ID yang memiliki tipe data INT dan hanya diisi bilangan bulat Contoh : 5
+    var IDStudent: Int = 5
 
     // Nama bertipe String, Contoh : Joko Tarbiah
+    var nama: String = "Joko Tarbiah"
 
     // Tanggal lahir bertipe String, contoh : 10 Agustus 2019
+    var tanggalLahir: String = "10 Agustus 2019"
 
     // fungsi yang mengubah format tanggal lahir menjadi standar tanggal dd-mm-yyyy. Contoh : 10-08-2019
+    fun tanggal(tanggal: String): String {
+        val formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", id)
+        val date = LocalDate.parse(tanggalLahir, formatter)
+        val hasil = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+
+        return hasil
+    }
 
     // Nomor handphone bertipe String yang memiliki syarat minimal panjang huruf 11 dan maksimal 12
+    var noHP: String = "085100000000"
 
     /*
     *  Orang tua bertipe Object yang berisi String nama kedua orang tua.
     *  Contoh : {"ayah" : "Stephen Sambura", "Ibu" : "Stephani Sambruang"}
     */
+    var orangTua = OrangTua("Stephen Sambura", "Stephani Sambruang")
 
     /*
     * Hobi yang bertipe array atau list atau arraylist yang berisikan string
     * contoh : ['Bersepeda', 'Bernyanyi', 'Makan']
     * */
+    var hobby =  mutableListOf("Bersepeda","Bernyanyi","Makan")
 
     /*
     * NIM yang memiliki tipe data String yang bersifat OTOMATIS di generate saat pembuatan object Student
@@ -31,8 +50,31 @@ class Student {
     * Tanggal Lahir : 10 Agustus 2019.
     * NIM nya menjadi : 5JT10082019
     * */
+    var nim: String = generateNIM(IDStudent,nama, tanggalLahir)
+    fun generateNIM(ID: Int, nama: String, tglLahir: String): String{
+        val inisial: String = nama
+            .trim()
+            .split(" ")
+            .mapNotNull { it.firstOrNull()?.toString() }
+            .reduce { acc, s -> acc + s }
+            .uppercase()
+        var tgl: String = tanggal(tglLahir)
+        tgl = tgl.replace("-","")
+        return "${ID.toString()}${inisial}${tgl}"
+    }
 
     /*
     * sebuah fungsi yang dapat mengembalikan nilai berbentuk ArrayList dengan tipe data Object Student
     * */
+}
+
+class OrangTua(val ayah: String,val ibu: String){ }
+
+fun main(args: Array<String>) {
+    val student:Student = Student()
+    println("Nama ${student.nama}")
+    println("NIM ${student.nim}")
+    println("Tanggal Lahir ${student.tanggalLahir}")
+    println("Hobby ${student.hobby}")
+
 }
--- src/main/kuis1/SalsabilaSyifa.kt
+++ src/main/kuis1/SalsabilaSyifa.kt
@@ -1,49 +1,60 @@
 /*
-SOAL
 1. Dewasa ini, pembuatan testing pada perangkat lunak umum dilakukan pada saat tahap implementasi yang dilakukan oleh software
    engineer. Berikan opini kalian apakah software engineer pada tahap implementasi wajib melakukan test atau tidak?
-
-2. Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi. berikan pendapat kalian jika dua test
-   ini tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
-
-3. Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer telah melakukan testing
-
-4. Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan. Namun tidak
-   semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan. Berikan pendapat kalian dampak
-   apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
-5. Berikan pendapat kalian tentang testing perangkat lunak secara manual? Apakah di saat banyaknya testing secara otomatisasi dilakukan testing secara
-    manual perlu dilakukan?
-
-
-JAWABAN
-1.  Kegagalan dalam melakukan pengujian perangkat lunak dapat menyebabkan produk yang dihasilkan tidak berjalan dengan baik dan
-    dapat menimbulkan kerugian ekonomi yang besar. Sebuah studi yang dilakukan oleh National Institute of Standart and Technologi (NIST)
-    pada tahun 2002 melaporkan bahwa bugs pada perangkat lunak menyebabkan kerugian ekonomi Amerika Serikat sebesar $59.5 billion tiap tahunnya,
-    dan sepertiga dari kerugian ini bisa dihindari jika dilakukan software testing yang lebih baik.
+   Jawab:
+    Menurut saya, sangat penting bagi software engineer dalam tahap implementasi untuk melakukan test. Karena jika tidak melakukan pengujian perangkat lunak,
+    produk yang dihasilkan tidak akan berfungsi dengan baik. Ini dapat menyebabkan kerugian finansial yang besar.
+    Berdasarkan Survei yang dilakukan oleh Institut Nasional Standar dan Teknologi (NIST)
+    Pada tahun 2002, dilaporkan bahwa kesalahan perangkat lunak menyebabkan kerugian ekonomi tahunan sebesar $59,5 miliar di Amerika Serikat.
+    Sepertiga dari kerugian ini dapat dihindari jika pengujian perangkat lunak yang lebih baik telah dilakukan.
     Tujuan utama dari pengujian perangkat lunak sebenarnya sederhana yaitu untuk memastikan bahwa software yang dihasilkan sesuai dengan kebutuhan
     (requirement) yang sebelumnya ditentukan. Ketika requirement dari suatu sistem telah disusun maka semestinya sudah ada suatu pengujian perencanaan
     (test plan). Selain itu suatu proses testing membutuhkan tujuan akhir yang dapat dinilai sehingga pihak tester bisa berhenti melakukan suatu
     testing ketika tujuan-tujuan itu tercapai.
+    referensi: https://se.ittelkom-pwt.ac.id/2017/10/25/pentingnya-melakukan-pengujian-perangkat-lunak-software-testing/
 
-2.  Unit Testing : Unit Testing adalah bagian terkecil yang dapat diuji dari seluruh aplikasi.
-    Tujuannya untuk memberikan sepotong kode yang harus memenuhi persyaratan.
-    Integration Testing : Dalam integration testing, kode akan dibagi menjadi beberapa segmen dan diuji sebagai kelompok.
-    Tugas utama integration testing adalah untuk menganalisis parameter seperti persyaratan fungsional, persyaratan kinerja dan persyaratan keandalan yang ditempatkan pada item desain utama.
+2. Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi. berikan pendapat kalian jika dua test
+   ini tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
+   Jawab:
+    Unit Testing: Tes unit adalah bagian terkecil dari aplikasi yang dapat uji. Tujuannya adalah untuk memberikan kode yang harus memenuhi persyaratan.
+    Integration Testing : Pengujian integrasi: Dalam pengujian integrasi, kode dibagi menjadi beberapa bagian dan diuji sebagai grup. Tugas utama pengujian
+    integrasi adalah menganalisis parameter seperti persyaratan fungsional, kinerja, dan keandalan yang ditempatkan pada elemen desain utama.
+    Jadi jika tidak kita lakukan hal tersebut, seperti pendapat saya sebelumnya, maka kemungkinan produk yang dihasilkan tidak akan berfungsi dengan baik.
+    referensi: https://socs.binus.ac.id/2020/06/30/software-testing/
 
-3.  Penguji Perangkat Lunak  mengeksplorasi  cara untuk memeriksa cara kerja aplikasi dan menemukan kemungkinan cacat. Berbagai metode dan
+3. Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer telah melakukan testing
+   Jawab:
+    Penguji Perangkat Lunak  mengeksplorasi  cara untuk memeriksa cara kerja aplikasi dan menemukan kemungkinan cacat. Berbagai metode dan
     metodologi pengujian digunakan oleh penguji untuk menguji produk, menemukan bug, dan memeriksa apakah telah diperbaiki. Pengujian memungkinkan
     pelanggan dengan kemungkinan untuk melihat apakah produk yang dikembangkan memenuhi harapan mereka pada desain, kompatibilitas, fungsi, dll.
     Fokus utama Penguji adalah menguji aplikasi dan menemukan semua kemungkinan bug yang dapat merusak aplikasi.
     Sementara di sisi lain, Insinyur QA  mengikuti serangkaian metode dan aktivitas yang dirancang untuk memastikan bahwa aplikasi perangkat lunak sesuai standar
     atau SRS dan sesuai dengan semua spesifikasi. Ini adalah strategi yang direncanakan dari evaluasi proses pengujian yang ditujukan untuk hasil produk yang berkualitas.
-    Insinyur QA mencari cara untuk  mencegah kemungkinan bug  dalam proses pengembangan perangkat lunak. Mereka juga menangani hal-hal manajemen seperti metode
+    Insinyur QA mencari cara untuk  mencegah kemungkinan bug dalam proses pengembangan perangkat lunak. Mereka juga menangani hal-hal manajemen seperti metode
     dan teknik pengembangan, analisis proyek, daftar periksa, dll. QA melewati seluruh siklus hidup produk (SDLC) dan memimpin proses pemeliharaan perangkat lunak.
     Fokus utama mereka adalah Kontrol Kualitas dan untuk memastikan persyaratan terpenuhi dengan benar.
+    referensi:  https://fintelics.medium.com/software-developer-vs-qa-engineer-tester-aeeea11a59ce
+                https://www.edureka.co/community/56196/difference-software-engineer-quality-assurance-engineers
 
-4.
+4. Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan. Namun tidak
+   semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan. Berikan pendapat kalian dampak
+   apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
+   Jawab:
+    Pengujian perangkat lunak adalah proses mengevaluasi dan memverifikasi bahwa produk perangkat lunak atau aplikasi melakukan apa yang seharusnya
+    dilakukan. Manfaat pengujian termasuk mencegah bug, mengurangi biaya pengembangan, dan meningkatkan kinerja.
+    berdasarkan pendapat dari berbagai sumber, test business logic ini sangat penting karena memungkinkan penguji untuk memperbaiki semua ketidakakuratan di awal proses pengujian QA
+    ketika kesalahpahaman logis hanya ada di atas kertas. Akhirnya, ini akan menghemat waktu pengembang, membuat proses pengembangan lebih efisien dan cepat.
+    Disisi lain, UI / UX memainkan peran integral dalam membantu aplikasi agar menonjol. Oleh karena itu, memiliki UI yang terintegrasi dan harmonis atau
+    UX dalam aplikasi seluler tidak hanya membantu bisnis untuk menarik lebih banyak pengguna tetapi juga meningkatkan kepuasan pelanggan.
+    referensi:  https://www.ibm.com/topics/software-testing#:~:text=Software%20testing%20is%20the%20process,development%20costs%20and%20improving%20performance.
+                https://testfort.com/blog/business-logic-in-qa-and-its-role-in-the-development-of-perfect-software#:~:text=Business%20Logic%20and%20Testing%20Documentation&text=This%20step%20is%20crucial%20because,process%20more%20efficient%20and%20fast.
 
-5.  Ada beberapa perbedaan utama antara pengujian otomatis vs. pengujian manual. Dalam pengujian manual, manusia melakukan pengujian langkah
+5. Berikan pendapat kalian tentang testing perangkat lunak secara manual? Apakah di saat banyaknya testing secara otomatisasi dilakukan testing secara
+   manual perlu dilakukan?
+   Jawab:
+    Tentunya perlu karena ada beberapa perbedaan utama antara pengujian otomatis dan pengujian manual. Dalam pengujian manual, manusia melakukan pengujian langkah
     demi langkah, tanpa skrip pengujian. Dalam pengujian otomatis, pengujian dijalankan secara otomatis melalui kerangka kerja otomatisasi pengujian,
     bersama dengan alat dan perangkat lunak lainnya.
     Meskipun pengujian manual tidak selalu seakurat otomatisasi, proses manual memungkinkan penguji memiliki lebih banyak fleksibilitas dalam operasinya.
+    referensi: https://www.perfecto.io/blog/automated-testing-vs-manual-testing-vs-continuous-testing
  */
