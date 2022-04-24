c679015cee9fe380c80d854c51eb01ad6175637e
ArinIhsan <arinihsan9@gmail.com>
2022-04-24 17:31:13 +0800

    quiz 1 pun ku

--- src/Quis1/Muhammad Ammarin Ihsan.kt
+++ src/Quis1/Muhammad Ammarin Ihsan.kt
@@ -4,13 +4,37 @@ Dewasa ini, pembuatan testing pada perangkat lunak umum dilakukan
 pada saat tahap implementasi yang dilakukan oleh software engineer.
 Berikan opini kalian apakah software engineer pada tahap implementasi wajib melakukan test atau tidak?
 
+Jawban:
+
+Berdasarkan dari beberapa sumber yang Saya baca di internet seperti penjelasan yang ada di website https://www.ibm.com/topics/software-testing
+menjelaskan bahwa dengan melakukan testing pada software yang dibuat maka kita dapat mendeteksi bug - bug yang ada dan memperbaiki
+kekurangan - kekurangan yang kita temui pada saat melakukan testing sebelum software tersebut pasarkan. Dengan demikian
+ketika software yang minim bug dan berkualitas tersebut dipasarkan maka akan mendapat respon positif dari client. Jadi menurut
+Saya lebih baik melakukan software testing daripada tidak dan jika ingin membuat software yang berkualitas tinggi maka
+hal tersebut merupakan kewajiban dalam tahap pembuatannya.
+
+
 No 2.
 Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi.
 berikan pendapat kalian jika dua test ini tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
 
+Jawaban:
+
+Berdasarkan website https://circleci.com/blog/unit-testing-vs-integration-testing/ menjelaskan bahwa
+unit test adalah testing yang dilakukan dengan mengisolasi seperti class atau sebuah fungsi, sedangkan
+integration test adalah testing yang dilakukan dengan mencek berbagai modul apakah berjalan baik satu sama lain.
+Dampak yang disebabkan jika kedua test ini tidak dilakukan maka akan menyebabkan beberapa fungsi atau class dan yang
+berinteraksi satu sama lain tidak terdeteksi apakah terjadi kesalahan dan ketika dipublish dengan kondisi tersebut
+mendapati banyak bug maka reputasi pembuat akan menurun.
+
 No 3.
 Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer
 telah melakukan testing
+
+Dari website https://u-tor.com/topic/what-does-qa-engineer-do disebutkan bahwa QA Engineer berfokus pada improvisasi
+kualitas software dan mencegah kecacatan pada software dengan berspesialisasi di bidang testing. Pekerjaan yang dilakukan
+software engineer lebih mengarah pada peningkatan kualitas pada software yang bisa dianalogikan sebagai pencegah kecacatan
+sedangkan testing yang dilakukan software engineer lebih ke diagnosis.
 
 No 4.
 Terdapat banyak test pada perangkat lunak, mulai dari test business logic,
@@ -18,7 +42,16 @@ UI, UX, Performa, sampai keamanan. Namun tidak semuanya dapat dilakukan karena
 keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan. Berikan pendapat
 kalian dampak apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
 
+Berdasarkan sumber yang saya baca pada https://www.ibeta.com/why-the-cost-of-software-testing-is-totally-worth-it/
+Dapat Saya simpulkan bahwa biaya yang dikeluarkan pada testing biasanya tidak melebihi keuntungan yang didapat ketika
+software dipublish. Akan tetapi hal ini tidak menutup kemungkinan untuk melakukan testing dengan budget terbatas.
+Dengan dilakukannya software testing maka kita dapat memperbaiki kekurangan - kekurangan yang ada pada software.
+
 No 5.
 Berikan pendapat kalian tentang testing perangkat lunak secara manual? Apakah di saat
 banyaknya testing secara otomatisasi dilakukan testing secara manual perlu dilakukan?
+
+Berdasarkan blog https://performancelabus.com/manual-testing-in-software-development/ dijelaskan bahwa manual testing
+memastikan kebutuhan client pada software terpenuhi dengan melakukan testing yang bersifat spesifik. Manual testing
+juga dapat mendeteksi kekurangan yang biasanya otomatisasi testing tidak dapat melakukannya.
  */
