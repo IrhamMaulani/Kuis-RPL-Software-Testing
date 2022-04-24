9f3aed412ccdac0291ad9fbd65ba52f4a9035a94
ArinIhsan <arinihsan9@gmail.com>
2022-04-24 22:08:35 +0800

    Fix Tanggal v1

--- src/test/java/StudentTest.kt
+++ src/test/java/StudentTest.kt
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.BeforeEach
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 internal class StudentTest {
     private lateinit var student: Student
@@ -11,9 +15,25 @@ internal class StudentTest {
     }
 
     @Test
+    fun testTanggalLahir(){
+        val tanggal = student.tanggalLahir
+
+        val formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale("in", "ID"))
+        val date = LocalDate.parse(tanggal, formatter)
+        val expectation = date.format(DateTimeFormatter.ofPattern("d MMMM yyyy", Locale("in", "ID")))
+
+        assertEquals(expectation, tanggal, "Penulisan Tanggal Salah")
+    }
+
+    @Test
     fun testFormatTanggal(){
-        val tanggal = "10 Agustus 2019"
-        val hasil = student.tanggal(tanggal)
-        Assertions.assertEquals(hasil, "10-08-2019", "Hasil tidak sama")
+        val tanggal = student.tanggalLahir
+        val hasil = student.formatTanggal(tanggal)
+
+        val formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale("in", "ID"))
+        val date = LocalDate.parse(tanggal, formatter)
+        val expectation = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+
+        assertEquals(expectation, hasil, "Hasil tidak sama")
     }
 }
