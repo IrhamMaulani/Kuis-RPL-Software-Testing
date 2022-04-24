ceafc34b3416631d638bb4653cd78020bd76c20e
Al Chatra Syarifudin <101439279+Chatra11@users.noreply.github.com>
2022-04-24 20:54:31 +0800

    nambah fungsi dan class

--- src/main/java/Student.kt
+++ src/main/java/Student.kt
@@ -1,3 +1,4 @@
+import java.lang.StringBuilder
 import java.text.SimpleDateFormat
 
 class Student {
@@ -24,29 +25,36 @@ class Student {
     // Tanggal lahir bertipe String, contoh : 10 Agustus 2019
 
     // fungsi yang mengubah format tanggal lahir menjadi standar tanggal dd-mm-yyyy. Contoh : 10-08-2019
-    
-    fun noHp(Nomor :String ): String {
-        var nomor = Nomor
-        if(nomor.length == 11||nomor.length == 12) {
-            return "Valid"
+
+    fun noHp(Nomor :String = "086753102945" ): String {
+        if(Nomor.length == 11||Nomor.length == 12) {
+            var nomor = Nomor
         }
         return "No Salah"
     }
+    val nomor = noHp()
     // Nomor handphone bertipe String yang memiliki syarat minimal panjang huruf 11 dan maksimal 12
 
-    val orangTua = object {
-        var ayah = "Stephen Sambura"
-        var ibu = "Stephani Sambruang"
-    }
+    class orangTua (
+        var ayah:String ,
+        var ibu :String
+    )
+    val Orangtua = orangTua("stephen Sambura","Stephani Sambruang")
     
     /*
     *  Orang tua bertipe Object yang berisi String nama kedua orang tua.
     *  Contoh : {"ayah" : "Stephen Sambura", "Ibu" : "Stephani Sambruang"}
     */
-
-    private val hobi: List<String> =
-        listOf("Swimming", "Basketball", "Volleyball", "Cooking", "Running")
-    
+    fun tampilhobi(){
+        val a = StringBuilder()
+        val hobi: List<String> =
+            listOf("Swimming", "Basketball", "Volleyball", "Cooking", "Running")
+        for (list in hobi){
+            a.append(list).append(", ")
+        }
+        val Hobi = a.toString().subSequence(0,a.length-2)
+        println(Hobi)
+    }
     /*
     * Hobi yang bertipe array atau list atau arraylist yang berisikan string
     * contoh : ['Bersepeda', 'Bernyanyi', 'Makan']
@@ -59,7 +67,7 @@ class Student {
         val inisial = nama.trim().splitToSequence(" ").take(2).map { println(it); it.first().uppercase()}.joinToString("")
         return "$id$inisial$tgl"
     }
-    
+
     /*
     * NIM yang memiliki tipe data String yang bersifat OTOMATIS di generate saat pembuatan object Student
     * dan merupakan kode yang berupa gabungan ID, huruf pertama dan akhir dari nama student,
