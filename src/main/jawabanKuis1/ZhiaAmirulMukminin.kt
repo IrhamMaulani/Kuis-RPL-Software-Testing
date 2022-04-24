6bc4b0d486a8d89e4b1ae10d1304e466c07318e0
zhiaam <91610329+zhiaam@users.noreply.github.com>
2022-04-24 13:42:50 +0800

    Update ZhiaAmirulMukminin.kt
    Menambah jawaban

--- src/main/jawabanKuis1/ZhiaAmirulMukminin.kt
+++ src/main/jawabanKuis1/ZhiaAmirulMukminin.kt
@@ -8,4 +8,22 @@
 4.Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan. Namun tidak semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan. Berikan pendapat kalian dampak apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
 
 5.Berikan pendapat kalian tentang testing perangkat lunak secara manual? Apakah di saat banyaknya testing secara otomatisasi dilakukan testing secara manual perlu dilakukan?
- */
+
+
+Jawaban:
+1. Menurut pendapat saya, testing merupakan hal yang penting karena bermanfaat untuk meminimalkan kemungkinan kesalahan atau bug serta meningkatkan kualitas perangkat lunak tersebut.
+
+2. Unit testing adalah pengujian terhadap tiap bagian fungsional program untuk memastikan setiap komponen berjalan dan mengeluarkan hasil yang diinginkan. Sedangkan integration testing adalah pengujian bagian program yang telah berkaitan dengan bagian lain dari program. Keduanya ini penting untuk mengetahui apakah program berjalan sesuai yang diharapkan atau tidak.
+
+3. Tugas Quality Assurance Engineer adalah memastikan aplikasi berjalan sesuai dengan yang diinginkan klien.
+
+4. Seluruh rangkaian test pada perangkat lunak diperlukan untuk memastikan bahwa aplikasi berjalan sesuai keinginan pada segala bidangnya.
+
+5. Testing secara manual saat ini sudah mulai dilakukan secara otomatis, dan pengujian otomatis tersebut lebih efisien daripada testinng manual.
+
+Referensi:
+https://repository.dinus.ac.id/docs/ajar/MATERI_KULIAH_IMPLEMENTASI_DAN_PENGUJIAN_SISTEM.pdf
+http://wahyu_pratama.staff.gunadarma.ac.id/Downloads/files/56333/Testing+dan+IS+Pertemuan+1+-+Pendahuluan.pdf
+https://www.labana.id/view/memahai-software-testing-unit-test-integration-test-apa-perlu-dan-dampaknya/2016/06/27/?fullview
+https://ijsecs.ump.edu.my/images/archive/vol4-1/ijsecs.4.1.2018.1.0044.pdf
+*/
