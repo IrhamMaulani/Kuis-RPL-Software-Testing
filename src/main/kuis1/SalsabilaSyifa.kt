7b33a52ae57f8ed646a598f1d2b3db7ba71c9dd5
Salsabila Syifa <2010817320004@mhs.ulm.ac.id>
2022-04-24 16:39:42 +0800

    kuis1

---
+++ src/main/kuis1/SalsabilaSyifa.kt
@@ -0,0 +1,49 @@
+/*
+SOAL
+1. Dewasa ini, pembuatan testing pada perangkat lunak umum dilakukan pada saat tahap implementasi yang dilakukan oleh software
+   engineer. Berikan opini kalian apakah software engineer pada tahap implementasi wajib melakukan test atau tidak?
+
+2. Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi. berikan pendapat kalian jika dua test
+   ini tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
+
+3. Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer telah melakukan testing
+
+4. Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan. Namun tidak
+   semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan. Berikan pendapat kalian dampak
+   apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
+5. Berikan pendapat kalian tentang testing perangkat lunak secara manual? Apakah di saat banyaknya testing secara otomatisasi dilakukan testing secara
+    manual perlu dilakukan?
+
+
+JAWABAN
+1.  Kegagalan dalam melakukan pengujian perangkat lunak dapat menyebabkan produk yang dihasilkan tidak berjalan dengan baik dan
+    dapat menimbulkan kerugian ekonomi yang besar. Sebuah studi yang dilakukan oleh National Institute of Standart and Technologi (NIST)
+    pada tahun 2002 melaporkan bahwa bugs pada perangkat lunak menyebabkan kerugian ekonomi Amerika Serikat sebesar $59.5 billion tiap tahunnya,
+    dan sepertiga dari kerugian ini bisa dihindari jika dilakukan software testing yang lebih baik.
+    Tujuan utama dari pengujian perangkat lunak sebenarnya sederhana yaitu untuk memastikan bahwa software yang dihasilkan sesuai dengan kebutuhan
+    (requirement) yang sebelumnya ditentukan. Ketika requirement dari suatu sistem telah disusun maka semestinya sudah ada suatu pengujian perencanaan
+    (test plan). Selain itu suatu proses testing membutuhkan tujuan akhir yang dapat dinilai sehingga pihak tester bisa berhenti melakukan suatu
+    testing ketika tujuan-tujuan itu tercapai.
+
+2.  Unit Testing : Unit Testing adalah bagian terkecil yang dapat diuji dari seluruh aplikasi.
+    Tujuannya untuk memberikan sepotong kode yang harus memenuhi persyaratan.
+    Integration Testing : Dalam integration testing, kode akan dibagi menjadi beberapa segmen dan diuji sebagai kelompok.
+    Tugas utama integration testing adalah untuk menganalisis parameter seperti persyaratan fungsional, persyaratan kinerja dan persyaratan keandalan yang ditempatkan pada item desain utama.
+
+3.  Penguji Perangkat Lunak  mengeksplorasi  cara untuk memeriksa cara kerja aplikasi dan menemukan kemungkinan cacat. Berbagai metode dan
+    metodologi pengujian digunakan oleh penguji untuk menguji produk, menemukan bug, dan memeriksa apakah telah diperbaiki. Pengujian memungkinkan
+    pelanggan dengan kemungkinan untuk melihat apakah produk yang dikembangkan memenuhi harapan mereka pada desain, kompatibilitas, fungsi, dll.
+    Fokus utama Penguji adalah menguji aplikasi dan menemukan semua kemungkinan bug yang dapat merusak aplikasi.
+    Sementara di sisi lain, Insinyur QA  mengikuti serangkaian metode dan aktivitas yang dirancang untuk memastikan bahwa aplikasi perangkat lunak sesuai standar
+    atau SRS dan sesuai dengan semua spesifikasi. Ini adalah strategi yang direncanakan dari evaluasi proses pengujian yang ditujukan untuk hasil produk yang berkualitas.
+    Insinyur QA mencari cara untuk  mencegah kemungkinan bug  dalam proses pengembangan perangkat lunak. Mereka juga menangani hal-hal manajemen seperti metode
+    dan teknik pengembangan, analisis proyek, daftar periksa, dll. QA melewati seluruh siklus hidup produk (SDLC) dan memimpin proses pemeliharaan perangkat lunak.
+    Fokus utama mereka adalah Kontrol Kualitas dan untuk memastikan persyaratan terpenuhi dengan benar.
+
+4.
+
+5.  Ada beberapa perbedaan utama antara pengujian otomatis vs. pengujian manual. Dalam pengujian manual, manusia melakukan pengujian langkah
+    demi langkah, tanpa skrip pengujian. Dalam pengujian otomatis, pengujian dijalankan secara otomatis melalui kerangka kerja otomatisasi pengujian,
+    bersama dengan alat dan perangkat lunak lainnya.
+    Meskipun pengujian manual tidak selalu seakurat otomatisasi, proses manual memungkinkan penguji memiliki lebih banyak fleksibilitas dalam operasinya.
+ */
