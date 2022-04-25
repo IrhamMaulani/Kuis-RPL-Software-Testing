2f70f2edc057417cc677b6560d4b8c3d97fa04c0
Rin <flowsnzr@gmail.com>
2022-04-25 12:45:20 +0800

    cleaning

--- src/main/java/StudentController.kt
+++ src/main/java/StudentController.kt
@@ -20,11 +20,3 @@ class StudentController(id: Int, nama: String, tanggalLahir: String, nomorHp: St
         return pelajar.hobi(hobi).joinToString(", ")
     }
 }
-
-fun main() {
-    val student1 = StudentController(5,"Joko Tarbiah","10 Agustus 2019", "085312345678")
-    student1.getIdentity()
-    println("Tanggal Lahir: ${student1.getTanggalLahir()}")
-    println("NIM: ${student1.getNIM()}")
-    println("Hobi: ${student1.getHobi(listOf("Hobi1", "Hobi2", "hobi3"))}")
-}
