ca8d12ae0db57445182062a12901247dd54cc7d9
ZCosmicc <101439750+ZCosmicc@users.noreply.github.com>
2022-04-25 14:33:31 +0800

    Add files via upload

---
+++ src/main/java/quiz_1/Fariz Fadillah.kt
@@ -0,0 +1,62 @@
+package quiz_1
+
+class `Fariz Fadillah` {
+/*
+    1.      Menurut sumber yang saya baca, software bukanlah suatu produk yang bisa berdiri sendiri. Di dalamnya
+        terdapat berbagai komponen yang saling terintegrasi antara satu dan yang lainnya. Sebelum bisa dijual, setiap komponen
+        tersebut tentu harus dipastikan setiap kualitasnya, dan setiap komponen yang ada pada software pun harus dilakukan uji
+        coba terlebih dahulu. Suatu software harus melalui suatu proses yang dikenal dengan sebutan Unit Testing.
+            Dilansir dari laman https://www.guru99.com/unit-testing-guide.html, unit testing adalah suatu jenis software testing yang
+        dilakukan untuk menguji coba suatu bagian ataupun komponen yang ada pada software. Unit tersebut bisa berbentuk fungsi, kode, metode,
+        prosedur, modul, ataupun objek itu sendiri.
+            Dan bila ditanya apakah wajib atau tidaknya, menurut saya tentu saja tidak wajib namun apabila sudah ranah profesional, sudah sepastinya Unit Testing
+        harus dilakukan seperti yang sudah disinggung diatas bahwa unit testing sendiri adalah termasuk salah satu tahapan di dalam software development.
+        Umumnya, pengujian tersebut dilakukan sebelum dilakukan system integration testing. Unit testing dilakukan agar bisa memastikan setiap unit kode
+        yang terdapat pada software bisa berjalan sebagai mana mestinya. Dan dengan unit testing pertama, dapat membantu memperbaiki bug yang berada di awal
+        siklus software development dan juga agar bisa menghemat biaya. Kedua, dapat membantu developer dalam memahami basis kode dan juga memungkinkan mereka
+        untuk bisa melakukan perubahan secara cepat. Ketiga, untuk dokumentasi proyek. Terakhir, untuk membantu penggunaan kembali kode dalam proyek baru.
+
+        https://accurate.id/marketing-manajemen/unit-testing/#Apa_Itu_Unit_Testing
+
+    2.      Unit testing adalah suatu jenis software testing yang dilakukan untuk menguji coba suatu bagian ataupun komponen yang ada pada software. Unit tersebut
+        bisa berbentuk fungsi, kode, metode, prosedur, modul, ataupun objek itu sendiri. Sedangkan, system integration testing adalah pengujian yang dilaksanakan
+        di lingkungan hardware dan software agar bisa mengecek keseluruhan sistem.
+            Seperti yang sudah disampaikan sebelumnya, mengapa unit testing itu penting karena dengan adanya unit testing dapat membantu memperbaiki bug di awal siklus
+        software development, membantu developer untuk memahami basis kode lebih mudah, berfungsi sebagai dokumentasi proyek, juga membantu penggunaan kembali kode pada
+        proyek yang baru. Dan untuk mengapa system integration system itu penting karena tujuan utama testing ini adalah untuk menguji otomatisasi dari gabungan komponen
+        yang ada, serta ketergantungan antara satu sama lain. Pengujian ini dibutuhkan untuk membangun program sekaligus secara tahu apakah ada kesalahan dalam interface.
+        Dengan mengikuti setiap rangkaiannya, pengujian jadi lebih teratur dan tidak berbelit-belit. Sistem lain yang bisa diintegrasikan atau digabungkan juga bisa saja
+        eksternal, yang berarti dikembangkan oleh pihak luar.
+
+        https://accurate.id/marketing-manajemen/unit-testing/#Apa_Itu_Unit_Testing
+        https://glints.com/id/lowongan/unit-testing-adalah/#.YmViwdpBy00
+        https://glints.com/id/lowongan/system-integration-testing-sit-adalah/#.YmVh9dpBy00
+
+    3.      QA Engineer merupakan seorang profesional yang terjun langsung untuk menemukan dan memperbaiki bug didalam sebuah produk atau program sebelum perilisan nya, dan
+        berkolaborasi dengan para developer dalam masalah perbaikan tersebut jika diperlukan. Seorang QA Engineer memiliki tanggung jawab penuh untuk meninjau quality
+        specifications dan dokumen technical design untuk menyediakan feedback yang tepat dan bermakna, pembuatan test plans dan test cases secara detail, komprehensif dan juga
+        well-structured. Dan selanjutnya, memperkirakan, memprioritaskan, merencakan dan mengkoordinasikan aktivitas quality testing.
+            Menurut pendapat saya, tidak ada permasalahan dikarenakan pada QA Engineer itu sendiri merupakan pekerjaan yang memang secara khusus memiliki tujuan untuk melakukan
+        hal tersebut, walaupun memang disisi lain Software Engineer telah melakukan testing, dengan adanya seorang QA Enginner maka akan memudahkan para developer juga karena berkolaborasi
+        dengan seseorang yang memang dikhususkan untuk melakukan testing tersebut.
+        
+        https://resources.workable.com/qa-engineer-job-description#:~:text=A%20QA%20Engineer%20is%20a,medical%20devices%20and%20food%2Fbeverage.
+
+    4.      Pengujian perangkat lunak dilakukan untuk mengurangi resiko kegagalan dalam pengembangan software; pengujian perangkat lunak yang tidak memadai
+        (atau sama sekali tidak ada) akan meningkatkan kemungkinan komplikasi pada developer, ketidaksetujuan dari stakeholder, dan consumer experience yang buruk.
+        Oleh karena itu, pengujian perangkat lunak merupakan prioritas utama di antara tim development. Beberapa resiko umum yang akan terjadi bila software testing
+        tidak dilakukan secara seharusnya. Yaitu, pengeluaran dana yang berlebihan dalam pengembangan software, perilisan produk atau software menjadi terlambat,
+        ketidakpuasan pengguna, dan yang paling buruk, memungkinkan terjadinya pengurangan reputasi pada brand yang berkaitan.
+
+        https://www.ibeta.com/risks-of-not-testing-software-properly/#:~:text=The%20job%20of%20software%20testers,top%20priority%20among%20development%20teams.
+
+    5.      Ada berbagai macam perbedaan besar diantara automated testing dan manual testing. Pada manual testing, pelaksanaan testnya dilakukan secara langsung oleh manusia step by step,
+        tanpa menggunakan test scripts. Sedangkan pada automated testing, test dieksekusi secara otomatis via test automation frameworks, bersamaan dengan alat dan perangkat lunak lainnya.
+        Perbedaan utama dari kedua testing ini adalah siapa yang mengeksekusi test tersebut. Pada pengujian manual, seorang tester manusia yang melakukannya. Dan pada pengujian otomatisasi,
+        alat yang melakukannya. Dalam pengujian manual, tester mem-validasi key features dari sebuah software application. Analysts mengeksekusi test cases dan mendevelop ringkasan laporan
+        error tanpa alat otomatisasi khusus. Sedangkan pada pengujian otomatisasi, akan membantu tester menjalankan lebih banyak test cases dan meningkatkan cakupan pengujian. Saat membandingkan
+        manual testing vs otomatisasi, manual membutuhkan waktu lebih lama dan automated testing lebih efisien.
+
+        https://www.perfecto.io/blog/automated-testing-vs-manual-testing-vs-continuous-testing
+ */
+}
