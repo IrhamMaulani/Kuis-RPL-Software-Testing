b4c774a596d7067aa7e5920b3bac605fbb0579f3
Putri Syifa Amalia <101611794+Syifahzn@users.noreply.github.com>
2022-04-25 19:44:06 +0800

    Update StudentTest.kt

--- src/test/java/StudentTest.kt
+++ src/test/java/StudentTest.kt
@@ -3,5 +3,28 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.BeforeEach
 
 internal class StudentTest {
-  
+  private lateinit var student: Student
+
+    @BeforeEach
+    fun beforeEach(){
+        student = Student()
+    }
+
+    @Test
+    fun testFullIdentificationSuccess() {
+        val student = Student(2, "Ahmad Fulan", "10 Agustus 2019", "082112341234")
+        assertEquals(listOf("2", "Ahmad Fulan", "10 Agustus 2019", "082112341234"), student.getFullIdentification())
+    }
+
+    @Test
+    fun testGetNomorHpSuccess(){
+        val noHp = student.getNomorHp("085312345678")
+        assertEquals("085312345678", noHp)
+    }
+
+    @Test
+    fun testGetNomorHPFailed(){
+        val noHp = student.getNomorHp("0821")
+        assertEquals(noHp,"Nomor tidak valid")
+    }
 }
