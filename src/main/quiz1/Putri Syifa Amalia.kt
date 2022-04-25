d80a4010d1a69a326ed10c61b2c7a183de487e41
Putri Syifa Amalia <101611794+Syifahzn@users.noreply.github.com>
2022-04-25 15:35:39 +0800

    Add files via upload

---
+++ src/main/quiz1/Putri Syifa Amalia.kt
@@ -0,0 +1,68 @@
+/*
+1. Dewasa ini, pembuatan testing pada perangkat lunak umum dilakukan pada saat tahap implementasi yang dilakukan oleh software engineer.
+Berikan opini kalian apakah software engineer pada tahap implementasi wajib melakukan test atau tidak?
+Jawab :
+
+Menurut Sumber : https://socs.binus.ac.id/2020/06/30/software-testing/
+Software testing merupakan salah satu bagian penting dan sering digunakan dalam pengembangan dari suatu software. Dengan adanya software testing,
+software engineer mampu mengenali error atau kecacatan yang ada dari suatu software.Fitur-fitur utama pada software testing adalah verifikasi dan validasi.
+Software testing digunakan untuk menjawab pernyataan sebagai : Apakah software dapat berjalan atau berperilaku sebagaimana yang sudah ditentukan.
+Terdapat 5 fase software testing, yaitu requirement analysis, test case design and development, test execution, test closure, dan test process analysis.
+
+Menurut saya, software engineer pada tahap implementasi wajib melakukan test yang bertujuan untuk mengetahui apakah didalam software yang telah dibuat terdapat bug atau tidak.
+Sehingga dapat mengurangi atau bahkan tidak ada revisi ketika aplikasi sudah dirilis nantinya.
+
+2. Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi. berikan pendapat kalian jika dua test ini
+tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
+Jawab :
+
+Menurut Sumber : https://sis.binus.ac.id/2016/12/16/unit-pengujian/
+http://sendy.itmaranatha.org/PSI/RPL-7th_ed_software_engineering_a_practitioners_approach_by_roger_s._pressman_.pdf
+Unit test adalah tingkat pengujian perangkat lunak dimana masing-masing unit dari perangkat lunak yang diuji. tujuan unit test adalah untuk memvalidasi bahwa setiap unit perangkat
+lunak melakukan seperti yang sudah dirancang. Menurut Roger S.Pressman didalam bukunya yang berjudul Software Engineering A Practitioner’s Approach, Integration test adalah tingkat
+pengujian perangkat lunak dimana modul-modul yang berdiri sendiri digabungkan dan diuji sebagai sebuah kesatuan. integration test adalah sebuah teknik sistematis untuk membangun
+arsitektur sebuah perangkat lunak dan pada saat yang sama melakukan pengujian untuk mencari kesalahan (error) yang berhubungan dengan antarmuka.
+
+Menurut saya, jika dua test ini tidak dilakukan akan mengakibatkan semakin banyaknya biaya dan waktu yang diperlukan dalam proses deploy karena akan ditemukan kondisi bahwa program tidak
+berjalan dengan baik nantinya. hal ini akan menyita waktu untu revisi program lagi. oleh karena itu, unit test dan integration test penting untuk dilakukan agar tidak terlalu banyak waktu
+terbuang untuk revisi.
+
+3. Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer telah melakukan testing
+Jawab :
+
+Menurut Sumber : https://www.ekrut.com/media/8-skill-ini-kamu-butuhkan-untuk-menjadi-qa-engineer-handal
+QA Engineer (quality assurance engineer) kerap menjadi salah satu posisi yang penting di dalam sebuah perusahaan teknologi. Tugas profesi ini adalah untuk melakukan tes yang bertujuan untuk
+mencari masalah atau kelemahan dari suatu software sebelum diluncurkan ke pengguna.
+
+Menurut saya, QA engineer memiliki tugas yang berbeda secara spesifik dengan software engineer. QA engineer bertugas untuk melakukan tes terhadap software yang dikembangkan dan memiliki
+kekuasaan untuk menentukan apakah software tersebut sudah layak untuk dirilis.
+
+4. Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan.
+Namun tidak semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan.
+Berikan pendapat kalian dampak apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
+Jawab :
+
+Menurut Sumber : https://testfort.com/blog/business-logic-in-qa-and-its-role-in-the-development-of-perfect-software
+https://www.toptal.com/designers/ux/user-testing-methods
+https://www.guru99.com/performance-testing.html
+https://www.guru99.com/what-is-security-testing.html
+Business logic adalah sebuah test untuk memastikan bahwa software berjalan sesuai dengan seharusnya. Testing pada UI bertujuan untuk menguji ftur-fitur yang akan berinteraksi dengan user.
+UX testing bertujuan untuk menguji pada pengalaman user secara keseluruhan. Performa testing adalah proses pengujian software yang bertujuan untuk menguji performa pada software.
+Security testing adalah pengujian untuk menjaga dari macam resiko yang dapat ditimbulkan ketika menggunakan software tersebut, misalnya seperti penyusup.
+
+Menurut saya, sebaiknya sebisa mingkin untuk melakukan testing terhadap software yang dikembangkan, hal ini guna menghindari adanya kesalahan fatal dikemudian hari ketika aplikasi sudah
+dirilis yang justru akan semakin banyak menambah biaya perbaikan dan memakan waktu yang lebih banyak. Pengujian ini juga diharapkan dapat memperbaiki aplikasi sesuai dengan kebutuhan
+user dan dapat memeberikan kenyamanan kepada user saat menggunakan software tersebut.
+
+5. Berikan pendapat kalian tentang testing perangkat lunak secara manual? Apakah di saat banyaknya testing
+secara otomatisasi dilakukan testing secara manual perlu dilakukan?
+Jawab :
+
+Menurut Sumber : https://medium.com/dot-intern/manual-testing-vs-automated-testing-9244aaed1ed5#:~:text=1.Manual%20Testing,bantuan%20dari%20tools%20automated%20test.
+Pengujian manual adalah pengujian yang dilakukan secara manual oleh QA. Dalam pengujiannya QA memeriksa semua fitur pada software , untuk memastikan ada atau tidaknya bug dalam software.
+Biasanya pengujian secara manual dilakukan tanpa bantuan dari tools automated test. Manual testing berperan penting saat pengujian visual dimana automation tools tidak dapat melakukan.
+
+Menurut saya, pengujian secara manual masih diperlukan dalam pengujian software karena dengan pengujian manual akan didapatkan visual feedback yang cepat dan akan didapatkan feedback dari
+user sehingga akan diketahui apakah user menyukai software tersebut atau tidak. sedangkan jika hanya melakukan pengujian secara otomatis tidak akan mendapatkan feedback baik secara visual
+ataupun dari user.
+ */
