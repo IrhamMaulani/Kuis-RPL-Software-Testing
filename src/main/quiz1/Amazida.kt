f03feaab83a6d500a31abb19c32ca5934cc06042
lailyrhmh <lailyrhmh11@gmail.com>
2022-04-25 16:59:01 +0800

    update quiz 1

---
+++ src/main/quiz1/Amazida.kt
@@ -0,0 +1,51 @@
+package quiz1
+
+class Amazida {
+/*
+
+1. Dewasa ini, pembuatan testing pada perangkat lunak umum dilakukan pada saat tahap implementasi yang dilakukan oleh software
+engineer. Berikan opini kalian apakah software engineer pada tahap implementasi wajib melakukan test atau tidak?
+jawaban : Menurut saya melakukan test wajib dilakukan karena untuk mengetahui apakah software tersebut sudah sesuai
+dengan yang diharapkan atau tidak. Jika tidak melalukan testing, kesalahan apapun dapat terjadi pada software tersebut.
+
+2. Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi. berikan pendapat kalian jika dua test
+ini tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
+jawaban : unit test dan integration test sangat berpengaruh pada kualitas software yang dibuat. Unit test
+dilakukan untuk mencari kesalahan pada kode yang lebih spesifik sehingga jika terjadi kesalahan hanya perlu
+memperbaiki kode-kode tertentu. Integration test dilakukan untuk melakukan testing ketika running sistem. Jadi, kedua
+test ini saling dibutuhkan di saat-saat tertentu tergantung apa yang ingin diuji.
+
+3. Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer telah melakukan testing
+jawaban : Mengenai dua pekerjaan tersebut, QA Engineer maupun Software engineer(yang sudah melakukan testing) tentu memiliki tugas utama
+yang berbeda. Kedua tujuan dari tugas mereka memang sama yaitu untuk mengecek apakah terdapat bug didalam aplikasi, namun Software Engineer
+biasanya melalukan testing pada saat pengembangan saja atau pada saat software tersebut akan dirilis. Sedangkan QA Engineer bertanggung jawab
+untuk mencari bug dan mempertahankan kualitas hingga software tersebut dirilis, sehingga ketika software tersebut telah rilis QA Engineer juga
+harusnya memperhatikan customer experience.
+
+4. Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan. Namun tidak
+semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan. Berikan pendapat kalian dampak
+apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
+jawaban : Kembali lagi pada tujuan dari adanya software testing yaitu untuk memastikan apakah software yang sudah
+berjalan dengan baik dan sesuai dengan yang dibutuhkan, atau terdapat bug dalam sebuah software. Jika tidak melakukan
+testing dan terdapat bug didalam sebuah aplikasi, maka proses development harus diulang kembali karena akan sulit
+menemukan bug apalagi yg tidak teridentifikasi. Client akan mengemban banyak kerugian seperti kerugian dalam hal ekonomi,
+memakan banyak waktu, dsb.
+
+5. Berikan pendapat kalian tentang testing perangkat lunak secara manual? Apakah di saat banyaknya testing secara otomatisasi dilakukan testing secara
+manual perlu dilakukan?
+jawaban : Automation testing biasanya memang dapat mencari bug lebih cepat dan lebih banyak dibandingkan manual testing.
+Namun, tidak cukup sampai disitu terkadang pengujian yang berhubungan dengan tampilan software / UI juga diperlukan.
+Sehingga, diperlukan feedback dari tester yang berperan sebagai end-user untuk mengecek seluruh fitur dan tampilan
+karena mengecek UI tidak dapat menggunakan tools. Jadi, untuk masalah ketelitian dalam mencari bug, testing yang dilakukan
+secara otomatis tentu lebih baik dan jika ingin mendapat feedback dari manusia secara langsung maka manual testing perlu
+dilakukan.
+
+Daftar pustaka
+Aniche, M. (2022). Effective Software Testing: A Developer's Guide. Simon and Schuster.
+https://www.testim.io/
+Li, P. L., Ko, A. J., & Zhu, J. (2015, May). What makes a great software engineer?. In 2015 IEEE/ACM 37th IEEE International Conference on Software Engineering (Vol. 1, pp. 700-710). IEEE.
+https://medium.com/skyshidigital
+https://se.ittelkom-pwt.ac.id
+
+ */
+}
