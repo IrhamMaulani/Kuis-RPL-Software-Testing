317a52bca797faab3f864fe8a5ccb065a877bffe
Naftael Bagas <101777142+BagasSaputra321@users.noreply.github.com>
2022-04-25 16:24:37 +0800

    Create NaftaelBagasSaputra.kt

---
+++ src/main/Quiz1/NaftaelBagasSaputra.kt
@@ -0,0 +1,38 @@
+//Soal
+
+// 1. Dewasa ini, pembuatan testing pada perangkat lunak umum dilakukan pada saat tahap implementasi yang dilakukan oleh software engineer. 
+// Berikan opini kalian apakah software engineer pada tahap implementasi wajib melakukan test atau tidak?
+
+// 2. Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi. 
+// Berikan pendapat kalian jika dua test ini tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
+
+// 3. Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer telah melakukan testing
+
+// 4. Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan. 
+// Namun tidak semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan. 
+// Berikan pendapat kalian dampak apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
+
+// 5. Berikan pendapat kalian tentang testing perangkat lunak secara manual? 
+// Apakah di saat banyaknya testing secara otomatisasi dilakukan testing secara manual perlu dilakukan?
+
+// Jawaban:
+
+// 1. Menurut saya wajib melakukan tahap testing karena sebelum di implementasikan harus di cek apakah sesuai dengan permintaan dan dapat berjalan dengan baik atau tidak dengan adanya tahap testing maka developer tidak perlu mengulang dari awal jika terjadi kesalahan yang lebih fatal.
+
+// 2. Menurut saya jika tidak dilakukannya kedua testing baik unit maupun integration maka perangkat lunak yang kita bangun tidak maksimal karena tidak dilakukannya cek dan pengujian dan dampak yang diberikan pastinya perangkat lunak tidak maksimal dan akan banyak bug dan error pada pernagkat lunak.
+
+// 3. QA engineer dan software engineer dalam melakukan testing sama-sama memastikan perangkat berjalan dengan baik. Software Engineer mengeksplorasi cara untuk memeriksa cara kerja aplikasi dan menemukan kemungkinan cacat. Berbagai metode dan metodologi pengujian digunakan oleh penguji untuk menguji produk, menemukan bug, dan memeriksa apakah telah diperbaiki.Fokus utama Penguji adalah menguji aplikasi dan menemukan semua kemungkinan bug yang dapat merusak aplikasi. Sementara di sisi lain, QA Engineer mengikuti serangkaian metode dan aktivitas yang dirancang untuk memastikan bahwa aplikasi perangkat lunak sesuai standar atau SRS (Spesifikasi Kebutuhan Perangkat Lunak) dan sesuai dengan semua spesifikasi. Ini adalah strategi yang direncanakan dari evaluasi proses pengujian yang ditujukan untuk hasil produk yang berkualitas. Insinyur QA mencari cara untuk mencegah kemungkinan bug dalam proses pengembangan perangkat lunak. QA melewati seluruh siklus hidup produk (SDLC) dan memimpin proses pemeliharaan perangkat lunak. Fokus utama mereka adalah Kontrol Kualitas dan untuk memastikan persyaratan terpenuhi dengan benar.
+
+// 4. Menurut saya jika pengujian yang tidak diberikan bisa di kerjakan saat pengembangan selanjutnya dan tidak terlalu krusial, tidak mengapa untuk tidak melakukan testing tersebut. tetapi jika testing itu berguna untuk melakukan pengujian pada bug atau error atau keamanan maka sebaiknya testing tersebut yang didahulukan.
+
+// 5. Menurut saya testing manual diperlukan saat kita ingin mengetahui apakah ada human error pada perangkat lunak yang kita bangun, bisa saja terjad dimana source code tidak mengalami bug/error tetapi output yang kita inginkan tidak sesuai. testing secara otomatis memang membantu kita dalam mengecek apakah ada ada logical/syntax error namun tidak dapat menguji human error.
+
+//sumber:
+
+//1. https://glints.com/id/lowongan/software-testing-adalah/#.YmYx8tpBzIU
+
+//2. https://www.guru99.com/unit-test-vs-integration-test.html
+
+//3. https://www.quora.com/What-is-the-difference-between-a-QA-engineer-and-Software-Test-engineer
+
+//5. https://medium.com/skyshidigital/perbedaan-manual-testing-dan-automated-testing-9d13373a36e
