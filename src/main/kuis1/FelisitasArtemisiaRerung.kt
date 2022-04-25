9f7feaf4ab3eed5c78ccb12787eb0d65701dcadf
Felisitas <85219689+ListaRerung@users.noreply.github.com>
2022-04-25 09:18:07 +0800

    Create FelisitasArtemisiaRerung.kt

---
+++ src/main/kuis1/FelisitasArtemisiaRerung.kt
@@ -0,0 +1,39 @@
+/*
+
+Felisitas Artemisia Rerung
+2010817120001
+
+1.	Dewasa ini, pembuatan testing pada perangkat lunak umum dilakukan pada saat tahap implementasi yang dilakukan oleh software engineer.
+    Berikan opini kalian apakah software engineer pada tahap implementasi wajib melakukan test atau tidak?
+    Jawab: Menurut saya software engineer wajib melakukan test pada tahap implementasi ,
+    karena tahap implementasi adalah tahap terpenting pada sebuah pengembangan aplikasi tentu software engineer harus melakukan test untuk
+    memastikan tidak ada kesalahan dalam aplikasi dan pengembangan di tahap berikutnya menjadi lancar.
+    Referensi: https://www.guru99.com/software-testing-introduction-importance.html
+
+2.	Unit test dan integration test merupakan test yang dilakukan pada tahap implementasi.
+    Berikan pendapat kalian jika dua test ini tidak dilakukan beserta dampak yang akan terjadi pada perangkat lunak
+    Jawab: Jika kita tidak melakukan unit testing, kita tidak akan mengetahui apakah masing masing unit/ fitur dalam aplikasi kita berjalan
+    dengan baik atau tidak dan jika kita tidak melakukan integration testing, kita tidak akan mengetahui apakah kombinasi
+    antar unit / fitur berjalan lancar atau tidak. Dan akan berdampak buruk pada perangkat lunak yaitu error/bug yang tidak akan sulit diperbaiki
+    karena kita tidak melakukan test.
+    Referensi: https://medium.com/dot-intern/jenis-jenis-software-testing-a5bf2345c79d
+
+3.	Berikan pendapat kalian tentang pekerjaan QA Engineer sedangkan disisi lain Software Engineer telah melakukan testing
+    Jawab: QA engineer bekerja sebagai pengawas dari pengembangan suatu perangkat lunak, ia bertanggung jawab untuk memastikan
+    bahwa setiap bagian perangkat lunak telah memenuhi standar
+    Referensi: https://www.ekrut.com/media/jobs-directory/qa-engineer
+
+4.	Terdapat banyak test pada perangkat lunak, mulai dari test business logic, UI, UX, Performa, sampai keamanan.
+    Namun tidak semuanya dapat dilakukan karena keterbatasan waktu dan mahalnya biaya yang harus dikeluarkan.
+    Berikan pendapat kalian dampak apa yang terjadi jika pengujian diatas tidak diberikan pada perangkat lunak
+    Jawab: Tentu saja perangkat lunak masih bisa berjalan tanpa beberapa pengujian, tapi akan ada dampak yang mengikuti,
+    yaitu bisa terjadi error dalam perangkat lunak , yang mana ini akan sulit untuk diperbaiki jika perangkat lunak sudah dipasarkan
+    Referensi: https://se.ittelkom-pwt.ac.id/2019/02/20/software-testing-dalam-lingkup-software-engineering/
+
+5.	Berikan pendapat kalian tentang testing perangkat lunak secara manual?
+    Apakah di saat banyaknya testing secara otomatisasi dilakukan testing secara manual perlu dilakukan?
+    Jawab: Menurut saya sendiri, jika sudah dilakukan testing secar otomatis, maka tidak diperlukan lagi testing secara manual,
+    karena itu akan memakan biaya yang lebih dan menghabiskan banyak waktu.
+    Referensi: https://medium.com/dot-intern/manual-testing-vs-automated-testing-9244aaed1ed5
+
+*/
