9100a694ed451b33a4c121bd9d14933c218abea8
Putri Syifa Amalia <101611794+Syifahzn@users.noreply.github.com>
2022-04-25 19:40:36 +0800

    Update StudentController.kt

--- src/main/java/StudentController.kt
+++ src/main/java/StudentController.kt
@@ -1,15 +1,19 @@
 open class StudentController(id: Int, nama: String, tglLahir: String, noHp: String){
   val student = Student(id,nama,tglLahir,noHp)
   
-  fun getIdentity(){
+  fun getIdentity(): List<String> {
+    return student.getFullIdentification()
   }
   
-  fun getTanggalLahir(){
+  fun getTanggalLahir(tglLahir: String): String {
+    return student.formatTanggal(tglLahir)
   }
   
-  fun getNIM(){
+  fun getNIM(id: Int, nama: String, tglLahir: String): String {
+    return student.NIM(id, nama, tglLahir)
   }
   
-  fun getHobi(){
+  fun getHobi(hobi: List<String>): String {
+    return student.hobi(hobi).joinToString(", ")
   }
 }
