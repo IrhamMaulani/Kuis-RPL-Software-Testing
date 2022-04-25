aaa9cd8c60b20555b527596da1caf2c92becfdd6
Muhammad Aulia Akbar <101610494+royaleaulia@users.noreply.github.com>
2022-04-25 14:19:59 +0800

    Update Muhammad Aulia Akbar.kt

--- src/Quis1/Muhammad Aulia Akbar.kt
+++ src/Quis1/Muhammad Aulia Akbar.kt
@@ -28,4 +28,27 @@ Menurut Saya, jika tidak melakukan unit testing dan integration testing akan mem
 pengujian yang akan dilakukan disetiap unit kecil di dalam kode yang terdiri dari sangat banyak file kode dan semua integrasi antar file kode akan mempermudah kita 
 untuk mencari setiap kesalahan yang tidak terlihat dari file-file kode di dalam suatu projek besar. jika tidak dilakukan maka akan sangat sulit mencari kesalahan setiap
 unit yang ada karena banyaknya kode yang dibuat
+No.3
+Menurut saya, pekerjaan QA engineer tetap dibutuhkan walaupun sudah ada Software Engineer yang bisa melakukan testing, 
+kecuali perusahaan atau organisasi yang membuat Perangkat Lunak itu kekurangan biaya, maka dapat dipangkas dengan tidak adanya Quality Assurance.
+Namun sebaiknya QA engineer harus tetap ada agar kualitas Perangkat Lunak dapat maksimal dan tidak menimbulkan masalah. 
+QA engineer tidak hanya melakukan testing seperti Software Engineer yang melakukan testing pada implementasi dan hanya mencari bug dan errornya suatu kode,
+tugas QA Engineer lebih besar yaitu melakukan testing di semua lingkup perangkat lunak sejak tahap desain, development, hingga aplikasi rilis. 
+QA Engineering juga memastikan customer experience, seperti apakah usability suatu Perangkat Lunak sudah maksimal dan lain sebagainya. 
+No.4
+Menurut Saya dampak jika tida melakukan test dari business logic, UI, UX, Performa, sampai keamanan akan sangat besar, 
+perangkat lunak yang dibuat kemungkinan besar tidak akan dapat melakukan fungsinya, apalagi jika perangkat lunak yang di rekayasa sangat kompleks.
+Kebutuhan seperti business logic akan mempengaruhi kerja bisnis perusahaan. UI dan UX akan mempengaruhi kenyamanan user sebagai tampilan dari perangkat lunak,
+performa sangat dibutuhkan sebagai keandalan dari perangkat lunak agar bisa bersaing dengan perangkat lunak yang lain. 
+Keamanan akan menjadi citra dan harga diri Para software engineer dan perusahaan.
+Jika semua itu tidak dilakukan testing maka kemungkinannya perangkat lunak tidak akan berjalan sesuai dengan yang diharapkan. 
+Jika hal ini terjadi, Pembuatan perangkat lunak akan sia-sia. 
+Jadi menurut saya, walaupun terkendala biaya, testing harus tetap dilakukan walaupun dilakukan secara manual, 
+agar perangkat lunak yang direkayasa dapat digunakan sesuai fungsinya.
+No.5
+Menurut Saya, pengujian secara manual perlu dilakukan jika itu mengangkut tampilan UI / UX, 
+karena tampilan tersebut lebih mudah didapatkan dengan memberikan opini orang lain.
+Pengujian Manual juga dapat dilakuakn karena lebih murah daripada pengujian otomatis. biaya produksi dapat ditekan.
+Namun Pembuatan software terutama bagian back-end sebaiknya menggunakan pengujian otomatis, 
+karena dapat dengan mudah menemukan bug dan error daripada pengujian manual yang dilakukan oleh manusia
  */
