a68e6fab03e4ebfb9e2dd13a85717f596c687690
salsabilashofia <101611648+salsabilashofia@users.noreply.github.com>
2022-04-25 17:30:43 +0800

    Update Student.kt

--- src/main/java/Student.kt
+++ src/main/java/Student.kt
@@ -1,40 +1,46 @@
+import
+import
+
+
 class Student {
     
-    .
-    // Impelementasikan properties dan fungsi sesuai spesifikasi dibawah
-    // Dan buatkan test pada setiap fungsi yang memastikan bahwa fungsi berjalan baik.
-
-    // properties student berisikan :
-
-    // ID yang memiliki tipe data INT dan hanya diisi bilangan bulat Contoh : 5
-
-    // Nama bertipe String, Contoh : Joko Tarbiah
-
-    // Tanggal lahir bertipe String, contoh : 10 Agustus 2019
-
-    // fungsi yang mengubah format tanggal lahir menjadi standar tanggal dd-mm-yyyy. Contoh : 10-08-2019
-
-    // Nomor handphone bertipe String yang memiliki syarat minimal panjang huruf 11 dan maksimal 12
-
-    /*
-    *  Orang tua bertipe Object yang berisi String nama kedua orang tua.
-    *  Contoh : {"ayah" : "Stephen Sambura", "Ibu" : "Stephani Sambruang"}
-    */
-
-    /*
-    * Hobi yang bertipe array atau list atau arraylist yang berisikan string
-    * contoh : ['Bersepeda', 'Bernyanyi', 'Makan']
-    * */
-
-    /*
-    * NIM yang memiliki tipe data String yang bersifat OTOMATIS di generate saat pembuatan object Student
-    * dan merupakan kode yang berupa gabungan ID, huruf pertama dan akhir dari nama student,
-    * dan tanggal lahir : Contoh ID=5, nama : Joko Tarbiah,
-    * Tanggal Lahir : 10 Agustus 2019.
-    * NIM nya menjadi : 5JT10082019
-    * */
-
-    /*
-    * sebuah fungsi yang dapat mengembalikan nilai berbentuk ArrayList dengan tipe data Object Student
-    * */
+    var indonesia = Locale("in", "ID") // Format tanggal lahir menjadi standar tanggal
+    var id: Int // ID yang memiliki tipe data INT dan hanya diisi bilangan bulat
+    var nama: String // Nama bertipe String
+    var tglLahir: String // Tanggal lahir bertipe String
+    var noHp: String // Nomor handphone bertipe String
+    lateinit var namaAyah: String // Orang tua bertipe Object yang berisi String
+    lateinit var namaIbu: String // Orang tua bertipe Object yang berisi String
+    lateinit var NIM: String // NIM yang memiliki tipe data String
+    
+    fun getFullIdentification()
+    
+    //Mengubah menjadi format dd-mm-yyyy
+    fun formatTanggal() {
+        
+    }
+    
+    // Fungsi Nomor Hp
+    fun getNomorHp() {
+        
+    }
+    
+    // Fungsi Hobi
+    fun hobi() {
+        
+    }
+    
+    // Fungsi NIM
+    fun NIM() {
+        
+    }
+    
+    // Fungsi Orangtua
+    class ortu() {
+        
+    }
+    
+    // Fungsi yang dapat mengembalikan nilai berbentuk ArrayList dengan tipe data Object Student
+    val getStudentArrayList
+    
 }
