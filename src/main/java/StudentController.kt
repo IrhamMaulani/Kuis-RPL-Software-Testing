962679ccf65f356fb5e93fa2654fb19f7666f859
Putri Syifa Amalia <101611794+Syifahzn@users.noreply.github.com>
2022-04-25 17:39:22 +0800

    Create StudentController.kt

---
+++ src/main/java/StudentController.kt
@@ -0,0 +1,15 @@
+open class StudentController(id: Int, nama: String, tglLahir: String, noHp: String){
+  val student = Student(id,nama,tglLahir,noHp)
+  
+  fun getIdentity(){
+  }
+  
+  fun getTanggalLahir(){
+  }
+  
+  fun getNIM(){
+  }
+  
+  fun getHobi(){
+  }
+}
