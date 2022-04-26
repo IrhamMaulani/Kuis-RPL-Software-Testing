68ab755d67d5f59360b9c23e7c2344aa2df00e48
Annisa Mahfuzhah <2010817220018@mhs.ulm.ac.id>
2022-04-26 12:32:40 +0800

    Baru

--- src/main/java/Student.kt
+++ src/main/java/Student.kt
@@ -12,34 +12,49 @@ class Student {
     // ID yang memiliki tipe data INT dan hanya diisi bilangan bulat Contoh : 5
 
     var ID : Int = 0
+    constructor(ID : Int) {
+        this.ID =ID
+    }
 
     // Nama bertipe String, Contoh : Joko Tarbiah
 
-    var name : String = " "
+    var name : String = ""
+    constructor(ID : Int, name : String) {
+        this.name = name
+    }
 
     // Tanggal lahir bertipe String, contoh : 10 Agustus 2019
 
-    var tl : String = " "
+    var tanggallahir : String = ""
+    constructor(ID: Int, name: String, tanggallahir : String) {
+        this.tanggallahir = tanggallahir
+    }
 
     // fungsi yang mengubah format tanggal lahir menjadi standar tanggal dd-mm-yyyy. Contoh : 10-08-2019
    fun Tanggal() {
        val format = DateTimeFormatter.ofPattern("dd MMMM, yyyy", Locale.getDefault())
-        val date = LocalDate.parse(tl, format)
+        val date = LocalDate.parse(tanggallahir, format)
    }
 
     // Nomor handphone bertipe String yang memiliki syarat minimal panjang huruf 11 dan maksimal 12
     var nohp : String = ""
+    constructor(ID: Int, name: String, tanggallahir: String, nohp : String) {
+        this.nohp = nohp
+    }
     /*
     *  Orang tua bertipe Object yang berisi String nama kedua orang tua.
     *  Contoh : {"ayah" : "majdi", "Ibu" : "Stephani"}
     */
     var ortu : String = ""
+    constructor(ID: Int, name: String, tanggallahir: String, nohp: String, ortu : String) {
+        this.ortu = ortu
+    }
 
     /*
     * Hobi yang bertipe array atau list atau arraylist yang berisikan string
     * contoh : ['Bersepeda', 'Bernyanyi', 'Makan', 'Ke indomaret']
     * */
-    val hobi : listof("Bersepeda","Bernyanyi","Makan","Plastik","turu")
+//    val hobi : listof("Bersepeda","Bernyanyi","Makan","Plastik","turu")
 
     /*
     * NIM yang memiliki tipe data String yang bersifat OTOMATIS di generate saat pembuatan object Student
@@ -52,4 +67,5 @@ class Student {
     /*
     * sebuah fungsi yang dapat mengembalikan nilai berbentuk ArrayList dengan tipe data Object Student
     * */
+
 }
