c56ba5c04341d044a6bfef9c4158eb8fa41fa6d4
Morfz <muhammadmajdi2108@gmail.com>
2022-04-26 09:38:53 +0800

    Update MuhammadMajdi.kt

--- src/main/Quiz1/MuhammadMajdi.kt
+++ src/main/Quiz1/MuhammadMajdi.kt
@@ -1,43 +1,32 @@
 /*
-No. 1, Menurut website "https://www.sekawanmedia.co.id/blog/metode-software-development/" penting  untuk mencari
+1. Menurut website "https://www.sekawanmedia.co.id/blog/metode-software-development/" penting  untuk mencari
 kesalahan seperti bug, error ataupun permasalahan lain yang dapat muncul dari software tersebut.
 Sedangkan menurut pendapat saya lebih agak spesifik dan dijabarkan, tahapan pengujian (testing) ini wajib dilakukan
 dan kepentingannya tergantung oleh skala pula, semakin besar skala nya semakin besar tahapan pengujian tersebut,
 jika sudah menyangkut dengan perusahan yang besar dan mencakup skala yang besar pula maka saya katakan tahapan ini
 wajib dilakukan untuk menemukan banyak kesalahan sebelum digunakan oleh orang banyak dan apalagi yang dapat menimbulkan
 kerugikan  pada pihak lain dan pengujian ini lebih spesifiknya dilakukan oleh QA.
-No. 2, Menurut website "https://www.testim.io/blog/unit-test-vs-integration-test/" perbedaan unit test
-dan integration test ada pada tempat dan bagian pengujiannya maka,
-dari pendapat saya terkait referensi diatas, untuk unit test jika tidak dilakukan maka saat publishing akan
-banyak bug atau kesalahan yang lebih spesifiknya dibagian codebase yang berjalan di internal aplikasi, boros biaya,
-developer kurang memahami code dibanding yang melakukan testing. Untuk integration test agak sama tetapi kesalahannya
-yaitu akan banyak kesalahan di bagian service-service di luar sistem aplikasi kita misalnya yang berhubungan dengan
-REST API atau service thrift (service third party), kedua hal tersebut jika tidak dilakukan maka mungkin akan membuat
-ketidakakuratan aplikasi.
-No. 3, Dari website "https://glints.com/id/lowongan/apa-itu-quality-assurance/" yang menjabarkan mengenai pekerjaan
-QA dan dari hal tersebut saya dapat dapat mengambil kesimpulan dan serta pendapat bahwa tugas dari QA tidak hanya
-berfokuskan atau melakukan testing pada produk tersebut QA juga dapat melakukan hal lain seperti memastikan bahwa produk
-sudah dapat bekerja dengan baik dan memaparkan masalah apa saja yang perlu diperbaiki, QA juga dapat berfokus kepada
-quality and needs, meningkatkan efisiensi, membantu motivasi semua anggota agar dapat bekerja lebih baik, dll.
-No. 4,  Pada website "https://testfort.com/blog/business-logic-in-qa-and-its-role-in-the-development-of-perfect-software"
-menjelaskan peran pentingnya business logic pada software yaitu menentukan bagaimana data dapat ditampilkan, disimpan,
-dibuat, dan diubah. menyediakan sistem untuk memandu bagaimana objek bisnis bekerja satu sama lain.
-sedangkan untuk UI dan UX test yang saya baca dari website "https://www.toptal.com/designers/ux/user-testing-methods" ,
-Pengujian UI berfokus pada pengujian antarmuka pengguna situs web untuk memastikan semuanya berfungsi sebagaimana
-mestinya dan pengguna memahami cara menggunakan UI dan untuk UX test berfokus pada keseluruhan pengalaman pengguna dan
-bagaimana produk atau situs web membuat pengguna merasa.
-jadi untuk pendapat saya setelah membaca beberapa website tersebut dapat saya bayangkan dampak apa saja yang diberikan
-jika tidak mengimplementasikan test-test tersebut seperti :
-untuk test business logic jika tidak dilakukan maka software dapat pemanduan objeck bisnis tidak terpadu secara pasti,
-dan fungsi dari penampilan, menyimpanan, pembuatan, dan perubahan mungkin tidak berjalan semestinya,
-untuk UI dan UX, antarmuka pada software akan tidak pasti berjalan semestinya juga dan kita tidak tahu bagaimana
-pengalaman dan rasa pengguna terhadap software yang dibuat.
-untuk performa dan keamanan dapat saya bayangkan jika tidak dilakukan maka keefisienan software yang digunakan akan
-kurang dan keamanan software apalagi jika software menyangkut data pribadi ditakutkan akan di exploit karena bagi saya
-keamanan harus sering di uji dan kasarnya di bobol diri sendiri agar kita tahu agar bagaimana kita membuat sistem
-keamanan yang lebih kuat dan jika dibiarkan tidak diuji maka sistem keamanan pada software tersebut akan lemah
-begitu saja dan tidak terupdate.
-No. 5,  untuk menjawab apakah kita masih memerlukan testing secara manual setelah adanya testing secara otomatisasi kita
+2.	Perbedaan unit test dan integration test terdapat pada tempat serta bagian pengujiannya maka, menurut pendapat saya terkait pernyataan diatas, untuk unit test jika tidak dilakukan
+maka ketika publishing akan terdapat error maupun bug atau kesalahan, boros biaya, developer kurang paham code dibanding yang melakukan testing. Pada integration test agak sama namun
+kesalahannya yaitu akan banyak error di bagian service-service pada luar sistem aplikasi kita misalnya yang bekerjasama dengan REST api atau service thrift (service third party),
+kedua hal tersebut bila tidak dilakukan maka mungkin akan mengakibatkan ketidakakuratan aplikasi.
+Sumber: https://www.testim.io/blog/unit-test-vs-integration-test/
+3.	Pendapat saya mengenai QA Engineer yaitu berdasarkan sumber yang saya baca seorang Quality Assurance merupakan orang yang bertanggung jawab untuk memastikan software atau
+aplikasi yang diciptakan oleh sebuah perusahaan dapat bekerja dengan baik. Selain itu, seorang Quality Assurance juga bekerja untuk menjabarkan bagian mana dari sebuah
+software yang perlu diperbaiki. QA juga dapat membantu dalam meminimalisir terjadinya pengerjaan ulang, memotivasi anggota tim, meningkatkan kepercayaan konsumen, dan lain-lain.
+Sumber: https://glints.com/id/lowongan/apa-itu-quality-assurance/#.YmZNwU7P1PY
+4.  Menurut https://testfort.com/blog/business-logic-in-qa-and-its-role-in-the-development-of-perfect-software,
+therefore, business logic is the perfect means that helps QAs in achieving the main purpose of every testing strategy:
+make sure that an app does what it is designed and supposed to do. A step counter might have the cutest interface,
+but it is not useful if it doesn’t display a user walking activity or makes it not right.
+Integrating business logic deeply into the testing process allows testing teams to prevent such unpleasant issues.
+Menurut https://www.testim.io/blog/ui-testing-beginners-guide/, there are many reasons why UI testing is important.
+First of all, if your app has a faulty UI, your users won’t be able to perform the tasks they need. More often than not,
+the UI is the only way users can interact with the application. They can’t interact directly with the application’s
+internals and get what they want. So, a malfunctioning UI is an insurmountable obstacle for your users.
+Menurut penulis berdasarkan sumber yang telah dibaca, tes-tes tersebut berguna agar software dapat berjalan
+dengan baik dan user dapat menggunakannya tanpa merasakan kebingungan.
+5.  untuk menjawab apakah kita masih memerlukan testing secara manual setelah adanya testing secara otomatisasi kita
 perlu tahu apa saja kelebihan dari masing-masing test, disini saya membaca website
 "https://www.perfecto.io/blog/automated-testing-vs-manual-testing-vs-continuous-testing" untuk mengetahui apa kekurangan
 dan kelebihan dari masing-masing test,
