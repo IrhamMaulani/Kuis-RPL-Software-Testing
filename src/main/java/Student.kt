2524052850db68212dac9969c03bfe4f8409163c
dwinabelaa <dwinabela0910@gmail.com>
2022-04-27 16:35:21 +0700

    Memperbaiki Student.class

--- src/main/java/Student.kt
+++ src/main/java/Student.kt
@@ -3,22 +3,56 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-class Student {
+class Student() {
+
+    constructor(IDStudent: Int, nama: String, tanggalLahir: String, noHP: String) : this(){
+        this.IDStudent = IDStudent
+        this.nama = nama
+        this.tanggalLahir = tanggalLahir
+        this.noHP = noHP
+    }
     // Impelementasikan properties dan fungsi sesuai spesifikasi dibawah
     // Dan buatkan test pada setiap fungsi yang memastikan bahwa fungsi berjalan baik.
 
     // properties student berisikan :
 
     // ID yang memiliki tipe data INT dan hanya diisi bilangan bulat Contoh : 5
+    var IDStudent: Int = 0
+        get() = field
+        set(value) {
+            field = value
+        }
 
     // Nama bertipe String, Contoh : Joko Tarbiah
+    var nama: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
 
     // Tanggal lahir bertipe String, contoh : 10 Agustus 2019
+    var tanggalLahir: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
 
     // fungsi yang mengubah format tanggal lahir menjadi standar tanggal dd-mm-yyyy. Contoh : 10-08-2019
 
-    // Nomor handphone bertipe String yang memiliki syarat minimal panjang huruf 11 dan maksimal 12
+    fun tanggal(tanggal: String): String {
+        val formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", id)
+        val date = LocalDate.parse(tanggalLahir, formatter)
+        val hasil = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
 
+        return hasil
+    }
+    // Nomor handphone bertipe String yang memiliki syarat minimal panjang huruf 11 dan maksimal 12
+    var noHP: String = ""
+        get() = field
+        set(value) {
+            if (value.length<11||value.length>12) field = "No Hp Tidak Valid"
+            else field = value
+        }
     /*
     *  Orang tua bertipe Object yang berisi String nama kedua orang tua.
     *  Contoh : {"ayah" : "Stephen Sambura", "Ibu" : "Stephani Sambruang"}
@@ -29,6 +63,12 @@ class Student {
     * contoh : ['Bersepeda', 'Bernyanyi', 'Makan']
     * */
 
+    var nim: String = ""
+        get() = field
+        set(value) {
+            field = generateNIM(this.IDStudent, this.nama, this.tanggalLahir)
+        }
+
     /*
     * NIM yang memiliki tipe data String yang bersifat OTOMATIS di generate saat pembuatan object Student
     * dan merupakan kode yang berupa gabungan ID, huruf pertama dan akhir dari nama student,
@@ -37,62 +77,49 @@ class Student {
     * NIM nya menjadi : 5JT10082019
     * */
 
-    /*
-    * sebuah fungsi yang dapat mengembalikan nilai berbentuk ArrayList dengan tipe data Object Student
-    * */
-
-    var id = Locale("in", "ID")
-    var dfs = DateFormatSymbols(id)
-
-    var IDStudent: Int = 0
-        get() = field
-        set(value) {
-            field = value
-        }
-    var nama: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-    var tanggalLahir: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-    var noHP: String = ""
-        get() = field
-        set(value) {
-            if (value.length<11||value.length>12) field = "No Hp Tidak Valid"
-            else field = value
-        }
-
-    var nim: String = ""
-        get() = field
-        set(value) {
-            field = generateNIM(this.IDStudent, this.nama, this.tanggalLahir)
-        }
-    var orangTua = OrangTua("Stephen Sambura", "Stephani Sambruang")
-    var hobby =  mutableListOf("Bersepeda","Bernyanyi","Makan")
-
-    fun tanggal(tanggal: String): String {
-        val formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", id)
-        val date = LocalDate.parse(tanggalLahir, formatter)
-        val hasil = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
-
-        return hasil
-    }
-
     fun generateNIM(ID: Int, nama: String, tglLahir: String): String{
         var inisial: String = nama
-            .trim()
-            .split(" ")
-            .mapNotNull { it.firstOrNull()?.toString() }
-            .reduce { acc, s -> acc + s }
-            .uppercase()
+                .trim()
+                .split(" ")
+                .mapNotNull { it.firstOrNull()?.toString() }
+                .reduce { acc, s -> acc + s }
+                .uppercase()
         var tgl: String = tanggal(tglLahir)
         tgl = tgl.replace("-","")
         return "${ID.toString()}${inisial}${tgl}"
     }
+
+    /*
+    * sebuah fungsi yang dapat mengembalikan nilai berbentuk ArrayList dengan tipe data Object Student
+    * */
+
+    fun getListStudent(): ArrayList<Student> {
+        val listStudents = ArrayList<Student>()
+        var student1 = Student()
+        student1.IDStudent = 3
+        student1.nama = "Bachrul Uluum"
+        student1.noHP = "0812345678999"
+        student1.tanggalLahir = "02 Desember 2001"
+        student1.nim = student1.generateNIM(student1.IDStudent,student1.nama, student1.tanggalLahir)
+        listStudents.add(student1)
+
+        return listStudents
+    }
+
+    var id = Locale("in", "ID")
+
+
+
+
+
+
+
+    var orangTua = OrangTua("Stephen Sambura", "Stephani Sambruang")
+    var hobby =  mutableListOf("Bersepeda","Bernyanyi","Makan")
+
+
+
+
 }
 
 class OrangTua(val ayah: String,val ibu: String){
